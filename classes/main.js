f2ea2cbc3fb3ce6fb3c917dd6ee85fecd7f32f2c
ZachyDev <mosetizachary001@gmail.com>
2020-06-15 02:55:31 +0300

    Class methods

--- classes/main.js
+++ classes/main.js
@@ -3,6 +3,11 @@ class Car {
     constructor(carId) {
         this.carId = carId
     }
+    // methods
+    getCar() {
+        return `Car ID: ${ this.carId }`
+    }
 }
 let car = new Car('Premio');
-console.log(car.carId);
+// console.log(car.carId);
+console.log(car.getCar());
