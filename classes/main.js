67e068894142895c2ac5340541b796691c18239f
ZachyDev <mosetizachary001@gmail.com>
2020-06-15 03:01:44 +0300

    Inheritance in classes(use keyword extends'

--- classes/main.js
+++ classes/main.js
@@ -10,4 +10,11 @@ class Car {
 }
 let car = new Car('Premio');
 // console.log(car.carId);
-console.log(car.getCar());
+// console.log(car.getCar());
+
+// inheritance
+class Vehicle extends Car{
+
+}
+let vehicle = new Vehicle('Vehicle');
+console.log(vehicle.getCar());
